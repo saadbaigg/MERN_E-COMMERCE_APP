e917dca4a68985c9d72eb46992a08351aa05afad
7-baig <saadbaig7777@gmail.com>
2020-12-29 22:53:13 +0500

    added home screen

---
+++ frontend/src/components/Product/Product.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Product.module.css'
+
+const Product = () => {
+    return (
+        <div>
+            hello
+        </div>
+    )
+}
+
+export default Product
---
+++ frontend/src/screens/HomeScreen/HomeScreen.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Product from "../../components/Product/Product";
+import styles from "./HomeScreen.module.css";
+import { products } from "../../products";
+
+const HomeScreen = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Latest Products</h1>
+      <div className={styles.productsContainer}>
+        {products.map((item) => {
+          <Product
+            key={item._id}
+            img={item.image}
+            name={item.name}
+            rating={item.rating}
+            numReviews={item.numReviews}
+            price={item.price}
+          />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default HomeScreen;
