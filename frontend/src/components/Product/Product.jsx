a670f0cbf827804bfd364f238648e3ee24fa4339
7-baig <saadbaig7777@gmail.com>
2020-12-30 11:11:11 +0500

    added product items

--- frontend/src/components/Product/Product.jsx
+++ frontend/src/components/Product/Product.jsx
@@ -5,11 +5,13 @@ const Product = ({ key, img, name, rating, numReviews, price }) => {
   return (
     <div className={styles.card} key={key}>
       <img src={img} alt={name} />
-      <p>{name}</p>
-      <p>
-        {rating} from {numReviews} reviews
-      </p>
-      <h3>{price}</h3>
+      <div className={styles.cardBody}>
+        <p>{name}</p>
+        <p>
+          {rating} from {numReviews} reviews
+        </p>
+        <h2>${price}</h2>
+      </div>
     </div>
   );
 };
