6a937c4a5818f76f40697f29ecd0290d2c9a0e0d
7-baig <saadbaig7777@gmail.com>
2020-12-30 18:13:06 +0500

    styled product detail page

--- frontend/src/screens/ProductScreen/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen/ProductScreen.jsx
@@ -1,14 +1,51 @@
-import React from 'react'
-import styles from './ProductScreen.module.css'
-import { products } from '../../products'
+import React from "react";
+import { products } from "../../products";
+import { Link } from "react-router-dom";
+import Rating from "../../components/Rating/Rating";
+import styles from "./ProductScreen.module.css";
 
 const ProductScreen = ({ match }) => {
-    const product = products.find((p) => parseInt(p._id) === parseInt(match.params.id))
-    return (
-        <div className={styles.container}>
-            {product.name}
-        </div>
-    )
-}
+  const product = products.find(
+    (p) => parseInt(p._id) === parseInt(match.params.id)
+  );
+  const {
+    name,
+    image,
+    description,
+    price,
+    rating,
+    numReviews,
+    countInStock,
+  } = product;
+  return (
+    <div className={styles.container}>
+      <Link to="/">
+        <p className={styles.goBack}>Go back</p>
+      </Link>
 
-export default ProductScreen
+      <div className={styles.subContainer}>
+        <div className={styles.imgContainer}>
+          <img src={image} alt={name} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1>{name}</h1>
+          <hr />
+          <div className={styles.ratingContainer}>
+            <Rating value={rating} text={`${numReviews} reviews`} />
+          </div>
+          <hr />
+          <p className={styles.price}>Price: ${price}</p>
+          <hr />
+          <p className={styles.description}>{description}</p>
+        </div>
+        <div className={styles.finalContainer}>
+          <span><p>Price:</p><p>${price}</p></span>
+          <span><p>Status:</p><p>{countInStock > 0 ? "In Stock" : "Out of Stock"}</p></span>
+          <span><button disabled={countInStock > 0}>Add to cart</button></span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductScreen;
