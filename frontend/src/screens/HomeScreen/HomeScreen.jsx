eba20c841a8c3feeae29bc19074cadb0142f877b
7-baig <saadbaig7777@gmail.com>
2020-12-30 19:50:01 +0500

    fetched products api

--- frontend/src/screens/HomeScreen/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen/HomeScreen.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Product from "../../components/Product/Product";
+import axios from 'axios'
 import styles from "./HomeScreen.module.css";
-import { products } from "../../products";
 
 const HomeScreen = () => {
+
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchedData = async () => {
+      const { data } = await axios.get('/api/products')
+      setProducts(data)
+    }
+    fetchedData()
+  }, [])
+
   return (
     <div className={styles.container}>
       <h1>Latest Products</h1>
