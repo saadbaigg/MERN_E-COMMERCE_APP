8c9c2d3d26ecbb5614595431a95e816f44628cd6
7-baig <saadbaig7777@gmail.com>
2020-12-31 20:30:45 +0500

    created separate routes folder

--- backend/server.js
+++ backend/server.js
@@ -13,15 +13,6 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  let product = products.find((p) => p._id === parseInt(req.params.id));
-  res.json(product);
-});
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(
