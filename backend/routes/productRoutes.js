ce3fa1f073eb4b32bbe785c44c6a9b7c6ed8ec6b
7-baig <saadbaig7777@gmail.com>
2020-12-31 20:49:01 +0500

    added description header

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,11 +3,19 @@ import asyncHandler from 'express-async-handler'
 const router = express.Router()
 import Product from "../models/productModel.js"
 
+// @desc     fetch all products
+// @route    GET /api/products 
+// @access   Public
+
 router.get("/", asyncHandler(async (req, res) => {
   const products = await Product.find({})
 
   res.json(products)
 }))
+
+// @desc     fetch single product
+// @route    GET /api/products/:id
+// @access   Public
 
 router.get("/:id", asyncHandler(async (req, res) => {
   let product = await Product.findById(req.params.id)
@@ -17,7 +25,7 @@ router.get("/:id", asyncHandler(async (req, res) => {
   } else {
       res.status(404).json({ message: 'Product not found' })
   }
-  
+
 }))
 
 export default router
