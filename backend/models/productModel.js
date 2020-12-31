f7610a9c5710281c72dd44dc0cc7be0dc941b778
7-baig <saadbaig7777@gmail.com>
2020-12-31 18:37:20 +0500

    created rating schema

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,5 +1,13 @@
 import mongoose from 'mongoose'
 
+const reviewSchema = mongoose.Schema({
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+}, {
+    timestamps: true
+})
+
 const productSchema = mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
@@ -26,6 +34,7 @@ const productSchema = mongoose.Schema({
         type: String,
         required: true,
     },
+    reviews: [reviewSchema],
     rating: {
         type: Number,
         required: true,
@@ -51,6 +60,6 @@ const productSchema = mongoose.Schema({
     timestamps: true
 })
 
-const User = mongoose.model('User', productSchema)
+const Product = mongoose.model('Product', productSchema)
 
-export default User
+export default Product
