e128a18bf4a77d08716d291a66b016da59d6ceb8
7-baig <saadbaig7777@gmail.com>
2020-12-31 16:49:23 +0500

    modified package.json for modules

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,27 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const products = require('./data/products')
+import express from "express"
+import dotenv from "dotenv"
+import products from "./data/products.js"
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-    res.send('API is running...')
-})
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
 
-app.get('/api/products/:id', (req, res) => {
-    let product = products.find(p => p._id === parseInt(req.params.id))
-    res.json(product)
-})
+app.get("/api/products/:id", (req, res) => {
+  let product = products.find((p) => p._id === parseInt(req.params.id));
+  res.json(product);
+});
 
-app.listen(5000, console.log('server running on PORT 5000'))
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`server running IN ${process.env.NODE_ENV} mode on PORT ${PORT}`)
+);
