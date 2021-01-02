775405b857e8fbfce907e507b435955dc579cd59
7-baig <saadbaig7777@gmail.com>
2021-01-02 22:12:57 +0500

    integrated single product page with redux

--- frontend/src/screens/ProductScreen/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen/ProductScreen.jsx
@@ -1,19 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import Rating from "../../components/Rating/Rating";
-import axios from "axios";
+import { listProductDetails } from "../../redux/actions/productActions";
+import Loader from "../../components/Loader/Loader";
+import Message from "../../components/Message/Message";
 import styles from "./ProductScreen.module.css";
 
 const ProductScreen = ({ match }) => {
-  const [product, setProduct] = useState([]);
+  const dispatch = useDispatch();
+
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
 
   useEffect(() => {
-    const fetchedData = async () => {
-      const { data } = await axios.get(`/api/products/${match.params.id}`);
-      setProduct(data);
-    };
-    fetchedData();
-  }, []);
+    dispatch(listProductDetails(match.params.id));
+  }, [dispatch, match]);
 
   const {
     name,
@@ -29,36 +31,41 @@ const ProductScreen = ({ match }) => {
       <Link to="/">
         <p className={styles.goBack}>Go back</p>
       </Link>
-
-      <div className={styles.subContainer}>
-        <div className={styles.imgContainer}>
-          <img src={image} alt={name} />
-        </div>
-        <div className={styles.textContainer}>
-          <h1>{name}</h1>
-          <hr />
-          <div className={styles.ratingContainer}>
-            <Rating value={rating} text={`${numReviews} reviews`} />
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message />
+      ) : (
+        <div className={styles.subContainer}>
+          <div className={styles.imgContainer}>
+            <img src={image} alt={name} />
           </div>
-          <hr />
-          <p className={styles.price}>Price: ${price}</p>
-          <hr />
-          <p className={styles.description}>{description}</p>
+          <div className={styles.textContainer}>
+            <h1>{name}</h1>
+            <hr />
+            <div className={styles.ratingContainer}>
+              <Rating value={rating} text={`${numReviews} reviews`} />
+            </div>
+            <hr />
+            <p className={styles.price}>Price: ${price}</p>
+            <hr />
+            <p className={styles.description}>{description}</p>
+          </div>
+          <div className={styles.finalContainer}>
+            <span>
+              <p>Price:</p>
+              <p>${price}</p>
+            </span>
+            <span>
+              <p>Status:</p>
+              <p>{countInStock > 0 ? "In Stock" : "Out of Stock"}</p>
+            </span>
+            <span>
+              <button disabled={countInStock > 0}>Add to cart</button>
+            </span>
+          </div>
         </div>
-        <div className={styles.finalContainer}>
-          <span>
-            <p>Price:</p>
-            <p>${price}</p>
-          </span>
-          <span>
-            <p>Status:</p>
-            <p>{countInStock > 0 ? "In Stock" : "Out of Stock"}</p>
-          </span>
-          <span>
-            <button disabled={countInStock > 0}>Add to cart</button>
-          </span>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
