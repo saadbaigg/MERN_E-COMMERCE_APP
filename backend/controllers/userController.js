f0bc338cb4ecde120f4c97f184d068e279a0888c
7-baig <saadbaig7777@gmail.com>
2021-01-18 01:08:58 +0500

    added delete backend route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -37,21 +37,20 @@ const registerUser = asyncHandler(async (req, res) => {
   const user = await User.create({
     name,
     email,
-    password
+    password,
   });
 
-  if(user) {
+  if (user) {
     res.status(201).json({
       _id: user._id,
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: generateToken(user._id),      
-    })
+      token: generateToken(user._id),
+    });
   } else {
-    res.status(400).json({ message: 'Invalid User data' })
+    res.status(400).json({ message: "Invalid User data" });
   }
-
 });
 
 // @desc     get user profile
@@ -75,27 +74,26 @@ const getProfile = asyncHandler(async (req, res) => {
 // @route    PUT /api/user/profile
 // @access   Private
 const updateProfile = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.user._id)
+  const user = await User.findById(req.user._id);
 
-  if(user) {
-    user.name = req.body.name || user.name
-    user.email = req.body.email || user.email
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
     if (req.body.password) {
-      user.password = req.body.password
+      user.password = req.body.password;
     }
-    const updatedUser = await user.save()
+    const updatedUser = await user.save();
     res.json({
       _id: updatedUser._id,
       name: updatedUser.name,
       email: updatedUser.email,
       isAdmin: updatedUser.isAdmin,
       token: generateToken(updatedUser._id),
-    });    
+    });
   } else {
     res.status(404).json({ message: "No User Found" });
   }
-
-})
+});
 
 // @desc     get all users (for admin only)
 // @route    GET /api/users
@@ -103,11 +101,23 @@ const updateProfile = asyncHandler(async (req, res) => {
 const getUsers = asyncHandler(async (req, res) => {
   const user = await User.find({});
   if (user) {
-    res.json(user)
-  } 
-  else {
+    res.json(user);
+  } else {
     res.status(404).json({ message: "No Users Found" });
   }
 });
 
-export { authUser, registerUser, getProfile, updateProfile, getUsers };
+// @desc     get delete user
+// @route    GET /api/users/:id
+// @access   Private
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (user) {
+    await user.remove()
+    res.json({ message: "Deleted Successfully" });
+  } else {
+    res.status(404).json({ message: "No User Found" });
+  }
+});
+
+export { authUser, registerUser, getProfile, updateProfile, getUsers, deleteUser };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from "express"
 const router = express.Router()
-import { authUser, registerUser, getProfile, updateProfile, getUsers } from '../controllers/userController.js'
+import { authUser, registerUser, getProfile, updateProfile, getUsers, deleteUser } from '../controllers/userController.js'
 import { admin, protect } from '../middleware/authMiddleware.js'
 
 router.get("/", protect, admin, getUsers)
@@ -8,5 +8,6 @@ router.post("/", registerUser)
 router.post("/login", authUser)
 router.get("/profile", protect, getProfile)
 router.put("/profile", protect, updateProfile)
+router.delete("/:id", protect, admin, deleteUser)
 
 export default router
