95ad76ac15253b18a8b7b6f57369266a10aef516
7-baig <saadbaig7777@gmail.com>
2021-01-18 20:06:38 +0500

    created delete product backend route

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -22,4 +22,17 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 })
 
-export { getProducts, getProductById }
+// @desc     delete products
+// @route    DELETE /api/products/:id
+// @access   Private
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    await product.remove()
+    res.json({ message: "Deleted Successfully" });
+  } else {
+    res.status(404).json({ message: "No Product Found" });
+  }
+});
+
+export { getProducts, getProductById, deleteProduct }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,8 +1,10 @@
 import express from "express"
 const router = express.Router()
-import { getProducts, getProductById } from '../controllers/productController.js'
+import { admin, protect } from "../middleware/authMiddleware.js";
+import { getProducts, getProductById, deleteProduct } from '../controllers/productController.js'
 
 router.get("/", getProducts)
 router.get("/:id", getProductById)
+router.delete("/:id", protect, admin, deleteProduct);
 
 export default router
