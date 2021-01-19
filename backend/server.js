926ffbc3aa54c145dc34069627c7e15bae000987
7-baig <saadbaig7777@gmail.com>
2021-01-20 01:35:19 +0500

    integrated upload route

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from "express"
 import dotenv from "dotenv"
 import connectDB from './config/db.js'
@@ -22,6 +23,9 @@ app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
 app.use('/api/upload', uploadRoutes)
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 const PORT = process.env.PORT || 5000;
 
--- frontend/src/screens/EditProductScreen/EditProductScreen.jsx
+++ frontend/src/screens/EditProductScreen/EditProductScreen.jsx
@@ -1,13 +1,18 @@
+import axios from "axios";
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import Message from "../../components/Message/Message";
 import Loader from "../../components/Loader/Loader";
 import { useDispatch, useSelector } from "react-redux";
 import styles from "./EditProductScreen.module.css";
-import { listProductDetails, updateProduct } from "../../redux/actions/productActions";
+import {
+  listProductDetails,
+  updateProduct,
+} from "../../redux/actions/productActions";
 
 const EditProductScreen = ({ history, match }) => {
   const dispatch = useDispatch();
+
+  const [uploading, setUploading] = useState(false);
 
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
@@ -24,130 +29,159 @@ const EditProductScreen = ({ history, match }) => {
   const { product, loading, error } = productDetails;
 
   const updatedProductState = useSelector((state) => state.updateProduct);
-  const { updatedProduct, success, loading:updateLoading, error:updateError } = updatedProductState;
+  const {
+    updatedProduct,
+    success,
+    loading: updateLoading,
+    error: updateError,
+  } = updatedProductState;
 
   useEffect(() => {
-    if(!product.name || product._id !== match.params.id) {
-      dispatch(listProductDetails(match.params.id))
+    if (!product.name || product._id !== match.params.id) {
+      dispatch(listProductDetails(match.params.id));
     } else {
-      setName(product.name)
-      setPrice(product.price)
-      setImage(product.image)
-      setBrand(product.brand)
-      setCountInStock(product.countInStock)
-      setCategory(product.category)
-      setDescription(product.description)
+      setName(product.name);
+      setPrice(product.price);
+      setImage(product.image);
+      setBrand(product.brand);
+      setCountInStock(product.countInStock);
+      setCategory(product.category);
+      setDescription(product.description);
     }
   }, [dispatch, history, match.params.id, product]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(updateProduct({
-      _id: product._id,
-      name,
-      price,
-      image,
-      brand,
-      countInStock,
-      category,
-      description
-    }))
+    dispatch(
+      updateProduct({
+        _id: product._id,
+        name,
+        price,
+        image,
+        brand,
+        countInStock,
+        category,
+        description,
+      })
+    );
+  };
+
+  const imgUploader = async (e) => {
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append("image", file);
+    setUploading(true);
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      };
+      const { data } = await axios.post("/api/upload", formData, config);
+      setImage(data);
+      setUploading(false);
+    } catch (error) {
+      console.log(error);
+      setUploading(false);
+    }
   };
 
   return (
     <div className={styles.container}>
       <div className={styles.subContainer}>
-        {/* {loading ? (
-          <Loader width="30px" />
-        ) : editSuccess ? (
-          <Message variant="success" text="User Updated Successfully" />
-        ) : editError ? (
-          <Message variant="error"text="Something's wrong" />
-        ) : null} */}
-        { updateLoading ? <Loader /> : null }
-        { success ? <Message variant="success" text="Product Updated Successfully" /> : null }
-        { updateError ? <Message variant="error" text="Something's wrong" /> : null }
+        {updateLoading ? <Loader /> : null}
+        {success ? (
+          <Message variant="success" text="Product Updated Successfully" />
+        ) : null}
+        {updateError ? (
+          <Message variant="error" text="Something's wrong" />
+        ) : null}
         {loading ? (
           <div>
             <Loader />
-          </div> 
-         ) : (
-        <div className={styles.formContainer}>
-          <h1>Edit Product</h1>
-          <form>
-            <div className={styles.field}>
-              <label>Name</label>
-              <input
-                type="text"
-                name="name"
-                value={name}
-                autoComplete="off"
-                onChange={(e) => setName(e.target.value)}
-              />
-            </div>
-            <div className={styles.field}>
-              <label>Price</label>
-              <input
-                type="text"
-                name="price"
-                value={price}
-                autoComplete="off"
-                onChange={(e) => setPrice(e.target.value)}
-              />
-            </div>
-            <div className={styles.field}>
-              <label>Image</label>
-              <input
-                type="text"
-                name="image"
-                value={image}
-                autoComplete="off"
-                onChange={(e) => setImage(e.target.value)}
-              />
-            </div>
-            <div className={styles.field}>
-              <label>Brand</label>
-              <input
-                type="text"
-                name="brand"
-                value={brand}
-                autoComplete="off"
-                onChange={(e) => setBrand(e.target.value)}
-              />
-            </div>
-            <div className={styles.field}>
-              <label>Count in Stock</label>
-              <input
-                type="text"
-                name="countInStock"
-                value={countInStock}
-                autoComplete="off"
-                onChange={(e) => setCountInStock(e.target.value)}
-              />
-            </div>
-            <div className={styles.field}>
-              <label>Category</label>
-              <input
-                type="text"
-                name="category"
-                value={category}
-                autoComplete="off"
-                onChange={(e) => setCategory(e.target.value)}
-              />
-            </div>
-            <div className={styles.field}>
-              <label>Description</label>
-              <input
-                type="text"
-                name="description"
-                value={description}
-                autoComplete="off"
-                onChange={(e) => setDescription(e.target.value)}
-              />
-            </div>
-            <button onClick={handleSubmit}>Update</button>
-          </form>
-        </div>
+          </div>
+        ) : (
+          <div className={styles.formContainer}>
+            <h1>Edit Product</h1>
+            <form>
+              <div className={styles.field}>
+                <label>Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  value={name}
+                  autoComplete="off"
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className={styles.field}>
+                <label>Price</label>
+                <input
+                  type="text"
+                  name="price"
+                  value={price}
+                  autoComplete="off"
+                  onChange={(e) => setPrice(e.target.value)}
+                />
+              </div>
+              <div className={styles.field}>
+                <label>Image</label>
+                <input
+                  type="text"
+                  name="image"
+                  value={image}
+                  autoComplete="off"
+                  onChange={(e) => setImage(e.target.value)}
+                />
+              </div>
+              { uploading && <Loader width="30px" /> }
+              <div className={styles.imgUpload}>
+                <input type="file" name="file" onChange={imgUploader} />
+                <button>Browse</button>
+              </div>
+              <div className={styles.field}>
+                <label>Brand</label>
+                <input
+                  type="text"
+                  name="brand"
+                  value={brand}
+                  autoComplete="off"
+                  onChange={(e) => setBrand(e.target.value)}
+                />
+              </div>
+              <div className={styles.field}>
+                <label>Count in Stock</label>
+                <input
+                  type="text"
+                  name="countInStock"
+                  value={countInStock}
+                  autoComplete="off"
+                  onChange={(e) => setCountInStock(e.target.value)}
+                />
+              </div>
+              <div className={styles.field}>
+                <label>Category</label>
+                <input
+                  type="text"
+                  name="category"
+                  value={category}
+                  autoComplete="off"
+                  onChange={(e) => setCategory(e.target.value)}
+                />
+              </div>
+              <div className={styles.field}>
+                <label>Description</label>
+                <input
+                  type="text"
+                  name="description"
+                  value={description}
+                  autoComplete="off"
+                  onChange={(e) => setDescription(e.target.value)}
+                />
+              </div>
+              <button onClick={handleSubmit}>Update</button>
+            </form>
+          </div>
         )}
       </div>
     </div>
